50db1a2382a67edf97463864f33b7c8993f9ff1c
Stanimirovic <78273896+Stane034@users.noreply.github.com>
2022-03-19 11:42:39 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,13 @@
+--[[
+    ____ ___ ____ _  _ ____    ____ _  _ ___ _    _  _ _  _ _    ___  _   _ ___  ____ ____ ____ 
+    [__   |  |__| |\ | |___    |__| |\ |  |  |    |\ | |  | |    |__]  \_/  |__] |__| [__  [__  
+    ___]  |  |  | | \| |___    |  | | \|  |  |    | \| |__| |    |__]   |   |    |  | ___] ___] 
+    
+    [v] Version :  1.0
+    [x] Coded By : Stane
+    [d] Description : You have to change and obfuscate or tokenize this NUI Callback and Events if you want to be safe 100%
+]]
+
 RegisterServerEvent('anticheat:randomEvent')
 AddEventHandler('anticheat:randomEvent', function()
     local _source = source
